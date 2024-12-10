d9874d4149a3ea1b19d64fc61810fbbeed5f866f
musayyyib <musayyib.ahmed@gmail.com>
2024-12-10 19:08:21 +0500

    first lecture learning

---
+++ JSPrograms/firstProgram.js
@@ -0,0 +1,31 @@
+//printing
+/* console.log("Hello") */
+
+//variables
+
+const a = 1   //integer
+const b = "ahmed"  //string
+
+let x = 2.22
+
+console.log(a)
+console.log(typeof(a))
+console.log(b)
+console.log(typeof(b))
+console.log(x)
+console.log(typeof(x))
+
+// let and const on ES6
+
+let a2 = 2333.333
+console.log(a2)
+
+
+let value = true
+console.log(value)
+console.log(typeof(value))
+
+c = a + a2
+console.log(c)
+
+// we cannot redeclare variable with let KW but possible with var
